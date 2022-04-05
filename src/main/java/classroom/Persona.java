0deae785139bc91222e4390c0d82e22914f1c102
Daniel Ceballos <dceballosm@unal.edu.co>
2022-04-04 21:10:46 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,16 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula = 3;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+    }
+    
+    Persona(){
+    	this("", 0);
     }
 
     Persona(long cedula, String nombre) {
@@ -30,7 +33,8 @@ public class Persona {
     }
 
     Persona(String nombre) {
-        this.nombre = "";
+        this.nombre = nombre;
+        this.cedula = 1;
         totalPersonas++;
     }
     
